33caea329299896a058d3422897e1f3e9caffb05
terao <terao.tsuyoshi@gmail.com>
2025-05-21 07:16:40 +0900

    Firebaseの設定ファイルパスを修正

--- src/webapp/pages/index.js
+++ src/webapp/pages/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { auth, db } from '../../../firebase/config';
+import { auth, db } from '../firebase/config';
 import { onAuthStateChanged } from 'firebase/auth';
 import { collection, getDocs } from 'firebase/firestore';
 
